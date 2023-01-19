80b9c5221d72161d4e1cff17c595e433b9c8e9f9
Iago López <ilopezsmx2011@gmail.com>
2023-01-19 08:52:43 +0100

    Plantillas

--- src/genweb6/theme/theme/tinymce_templates/README.rst
+++ src/genweb6/theme/theme/tinymce_templates/README.rst
@@ -10,13 +10,21 @@ Update templates.py
 
     templates = [
         {
-          "title": "List",
-          "description": "Lorem Ipsum",
+          "title-ca": "Llistat",
+          "title-es": "Listado",
+          "title-en": "List",
+          "description-ca": "Lorem Ipsum",
+          "description-es": "Lorem Ipsum",
+          "description-en": "Lorem Ipsum",
           "url": "++theme++genweb6.theme/tinymce_templates/list.html"
         },
         {
-          "title": "Card Group",
-          "description": "Lorem Ipsum",
+          "title-ca": "Grup de targetes",
+          "title-es": "Grupo de tarjetas",
+          "title-en": "Card Group",
+          "description-ca": "Lorem Ipsum",
+          "description-es": "Lorem Ipsum",
+          "description-en": "Lorem Ipsum",
           "url": "++theme++genweb6.theme/tinymce_templates/card-group.html"
         }
     ]
--- src/genweb6/theme/theme/tinymce_templates/templates.py
+++ src/genweb6/theme/theme/tinymce_templates/templates.py
@@ -1,12 +1,117 @@
 templates = [
     {
-      "title": "List",
-      "description": "Lorem Ipsum",
-      "url": "/++theme++genweb6.theme/tinymce_templates/list.html"
+      "title-ca": "Destacat - Estàndar",
+      "title-es": "Destacado - Estándar",
+      "title-en": "Featured - Standard",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/estandar.html"
     },
     {
-      "title": "Card Group",
-      "description": "Lorem Ipsum",
-      "url": "/++theme++genweb6.theme/tinymce_templates/card-group.html"
-    }
+      "title-ca": "Destacat - Color",
+      "title-es": "Destacado - Color",
+      "title-en": "Featured - Color",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/color.html"
+    },
+    {
+      "title-ca": "Destacat - Contorn",
+      "title-es": "Destacado - Contorno",
+      "title-en": "Featured - Outline",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/contorn.html"
+    },
+    {
+      "title-ca": "Destacat - Degradat",
+      "title-es": "Destacado - Degradado",
+      "title-en": "Featured - Gradient",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/degradat.html"
+    },
+    {
+      "title-ca": "Destacat - Caixa",
+      "title-es": "Destacado - Caja",
+      "title-en": "Featured - Box",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/caixa.html"
+    },
+    {
+      "title-ca": "Destacat - Caixa degradat",
+      "title-es": "Destacado - Caja degradado",
+      "title-en": "Featured - Gradient box",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/caixa-degradat.html"
+    },
+    {
+      "title-ca": "Destacat - Caixa amb llista fons gris",
+      "title-es": "Destacado - Caja con lista fondo gris",
+      "title-en": "Featured - Box with gray background list",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/caixa-llista-gris.html"
+    },
+    {
+      "title-ca": "Destacat - Caixa amb llista fons verd",
+      "title-es": "Destacado - Caja con lista fondo verde",
+      "title-en": "Featured - Box with list green background",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/caixa-llista-verd.html"
+    },
+    {
+      "title-ca": "Destacat - Frase destacada",
+      "title-es": "Destacado - Frase destacada",
+      "title-en": "Featured - Featured phrase",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/frase.html"
+    },
+
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
+    {
+      "title-ca": "",
+      "title-es": "",
+      "title-en": "",
+      "url": "/++theme++genweb6.theme/tinymce_templates/destacat/.html"
+    },
 ]
