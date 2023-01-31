60de4284d50079bbdd93b4217a7176247c01002a
Iago López <ilopezsmx2011@gmail.com>
2023-01-31 11:51:20 +0100

    Footer ocultar no-folders si no hay elementos

--- src/genweb6/theme/theme/js/theme-concat.js
+++ src/genweb6/theme/theme/js/theme-concat.js
@@ -15692,6 +15692,12 @@ $(document).ready(function(){
     $('#portal-footer').addClass($('#footer-complementary').data('class'));
     $('#portal-footer').attr('style', '--bg-img-url: url(' + $('#footer-complementary').data('img') + ')');
 
+    /* Ocultar no-folders si no tiene elementos */
+    if($("#portal-footer .no-folders").length > 0){
+        if($("#portal-footer .no-folders > ul > li:not(.has_subtree)").length == 0){
+            $("#portal-footer .no-folders").hide();
+        }
+    }
 });
 
 /*  FIN footer.js */
