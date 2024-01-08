89e206a214c5d735321952174b9680ca7c272dbf
Iago López <ilopezsmx2011@gmail.com>
2024-01-08 11:47:47 +0100

    Traducciones tinymce

--- src/genweb6/theme/theme/js/tinymce/translations.js
+++ src/genweb6/theme/theme/js/tinymce/translations.js
@@ -5,6 +5,17 @@ tinymce.addI18n(
     "Header 4 with line below": "Encapçalament 4 amb línia avall",
     "Header 5 with line below": "Encapçalament 5 amb línia avall",
     "Header 6 with line below": "Encapçalament 6 amb línia avall",
+    "Text in 2 columns": "Text en 2 columnes",
+    "Text in 3 columns": "Text en 3 columnes",
+    "Arxiu PDF": "Arxiu PDF",
+    "Arxiu DOC": "Arxiu DOC",
+    "Arxiu PPT": "Arxiu PPT",
+    "Arxiu TXT": "Arxiu TXT",
+    "Arxiu XLS": "Arxiu XLS",
+    "Arxiu imatge": "Arxiu imatge",
+    "Arxiu vídeo": "Arxiu vídeo",
+    "Cadenat HTTPS": "Cadenat HTTPS",
+    "Descarrega fitxer": "Descarrega fitxer",
   }
 );
 
@@ -15,6 +26,17 @@ tinymce.addI18n(
     "Header 4 with line below": "Encabezado 4 con linea abajo",
     "Header 5 with line below": "Encabezado 5 con linea abajo",
     "Header 6 with line below": "Encabezado 6 con linea abajo",
+    "Text in 2 columns": "Texto en 2 columnas",
+    "Text in 3 columns": "Texto en 3 columnas",
+    "Arxiu PDF": "Fichero PDF",
+    "Arxiu DOC": "Fichero DOC",
+    "Arxiu PPT": "Fichero PPT",
+    "Arxiu TXT": "Fichero TXT",
+    "Arxiu XLS": "Fichero XLS",
+    "Arxiu imatge": "Fichero imagen",
+    "Arxiu vídeo": "Fichero vídeo",
+    "Cadenat HTTPS": "Candado HTTPS",
+    "Descarrega fitxer": "Descarga archivo",
   }
 );
 
@@ -25,5 +47,16 @@ tinymce.addI18n(
     "Header 4 with line below": "Header 4 with line below",
     "Header 5 with line below": "Header 5 with line below",
     "Header 6 with line below": "Header 6 with line below",
+    "Text in 2 columns": "Text in 2 columns",
+    "Text in 3 columns": "Text in 3 columns",
+    "Arxiu PDF": "PDF file",
+    "Arxiu DOC": "DOC file",
+    "Arxiu PPT": "PPT file",
+    "Arxiu TXT": "TXT file",
+    "Arxiu XLS": "XLS file",
+    "Arxiu imatge": "Image archive",
+    "Arxiu vídeo": "Video archive",
+    "Cadenat HTTPS": "HTTPS padlock",
+    "Descarrega fitxer": "Download file",
   },
 );
