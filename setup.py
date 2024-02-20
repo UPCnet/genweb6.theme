3b5800f5c278b4d29b138c50ec2680ccf437be78
Pilar Marinas <pilar.marinas@upcnet.es>
2024-02-20 13:30:58 +0100

    Back to development: 1.18

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.theme',
-    version='1.17',
+    version='1.18.dev0',
     description="Genweb 6 Theme package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
