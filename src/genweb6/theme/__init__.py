93ddcadbacaaf980510a7df48cc600a4e50fa995
Alberto Duran <alberto.duran@ithinkupc.com>
2026-01-14 09:28:59 +0100

    [FIX] Error log when reading rules.xml

--- src/genweb6/theme/__init__.py
+++ src/genweb6/theme/__init__.py
@@ -4,3 +4,21 @@ from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('genweb')
+
+
+# Apply patches to fix theme resource access in protected contexts
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+    from genweb6.theme.patches import apply_patches
+    apply_patches()
+
+
+# Apply patches immediately on import
+try:
+    from genweb6.theme.patches import apply_patches
+    apply_patches()
+except Exception:
+    # If patches fail to apply, log but don't break the import
+    import logging
+    logger = logging.getLogger('genweb6.theme')
+    logger.exception("Failed to apply genweb6.theme patches")
